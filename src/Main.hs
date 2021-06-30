91138e626797cf2f1fa4ba5448a2ae279dd1db7e
Pranshi <pranshi@hasura.io>
2021-06-30 16:08:45 +0530

    Rendering a form - doubt

--- src/Main.hs
+++ src/Main.hs
@@ -35,25 +35,6 @@ param' :: forall p (m :: * -> *) ctx.
   => Text
   -> ActionCtxT ctx m p
 
-app :: Server ()
-app = do 
-    get root $ do
-        notes' <- getState >>= (liftIO . readIORef . notes)
-        lucid $ do
-            h1_ "Notes"
-            ul_ $ forM_ notes' $ \note -> li_ $ do
-                toHtml (author note)
-                ": "
-                toHtml (contents note)
-    post root $ do
-        author <- param' "author"
-        contents <- param' "contents"
-        notesRef <- notes <$> getState
-        liftIO $ atomicModifyIORef' notesRef $ \notes ->
-            (notes <> [Note author contents], ())
-        redirect "/"
-
--- ----------------------- Rendering a Form----------------
 -- app :: Server ()
 -- app = do 
 --     get root $ do
@@ -64,15 +45,41 @@ app = do
 --                 toHtml (author note)
 --                 ": "
 --                 toHtml (contents note)
---             h2_ "New Note"
---             form_ [method_ "post"] $ do
---                 label_ $ do
---                     "Author: "
---                     input_ [name_ "author"]
---                 label_ $ do
---                     "Contents: "
---                     textarea_ [name_ "contents"] ""
---                 input_ [type_ "submit", value_ "Add Note"]
+--     post root $ do
+--         author <- param' "author"
+--         contents <- param' "contents"
+--         notesRef <- notes <$> getState
+--         liftIO $ atomicModifyIORef' notesRef $ \notes ->
+--             (notes <> [Note author contents], ())
+--         redirect "/"
+
+-- ----------------------- Rendering a Form----------------
+app :: Server ()
+app = do 
+    get root $ do
+        notes' <- getState >>= (liftIO . readIORef . notes)
+        lucid $ do
+            h1_ "Notes"
+            ul_ $ forM_ notes' $ \note -> li_ $ do
+                toHtml (author note)
+                ": "
+                toHtml (contents note)
+            h2_ "New Note"
+            form_ [method_ "post"] $ do
+                label_ $ do
+                    "Author: "
+                    input_ [name_ "author"]
+                label_ $ do
+                    "Contents: "
+                    textarea_ [name_ "contents"] ""
+                input_ [type_ "submit", value_ "Add Note"]
+    post root $ do
+        author <- param' "author"
+        contents <- param' "contents"
+        notesRef <- notes <$> getState
+        liftIO $ atomicModifyIORef' notesRef $ \notes ->
+            (notes <> [Note author contents], ())
+        redirect "/"
 
 ----------------------------------------------------
 
