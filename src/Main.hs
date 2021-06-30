6e0a412f3d21d57c243a4386b48045834a5ac1ca
Pranshi <pranshi@hasura.io>
2021-06-30 15:33:50 +0530

    Posting new notes with a form - error

--- src/Main.hs
+++ src/Main.hs
@@ -19,64 +19,6 @@ import Control.Monad.IO.Class (liftIO, MonadIO)
 import Data.Semigroup ((<>))
 import Servant.API
 
--- -------------- Show notes -----------------
--- app :: SpockM () () () ()
--- app = return ()
-
--- type Server a = SpockM () () () a
-
--- app :: Server ()
--- app = return ()
-
--- Defining Routes
--- app :: Server ()
--- app = get root (text "Hello!")
-
--- Rendering HTML
--- app :: Server ()
--- app = get root (html "<h1>Hello!</h1>")
-
--- Lucid
--- app :: Server ()
--- app = get root $ lucid $ do
---     h1_ "Hello!"
---     p_ "How are you today?"
-
--- main :: IO ()
--- main = do
---     -- let app = return ()
---     cfg <- defaultSpockCfg () PCNoDatabase ()
---     runSpock 8080 (spock cfg app)
-
--- ----------- Keeping Notes with Server State -------------------
-
--- newtype ServerState = ServerState { notes :: IORef [Note] }
--- data Note = Note {
---     author      :: Text,
---     contents    :: Text
--- }
-
--- type Server a = SpockM () () ServerState a
-
--- app :: Server ()
--- app = get root $ do
---     notes' <- getState >>= (liftIO . readIORef . notes)
---     lucid $ do
---         h1_ "Notes"
---         ul_ $ forM_ notes' $ \note -> li_ $ do
---             toHtml (author note)
---             ": "
---             toHtml (contents note)
-
-
--- main :: IO ()
--- main = do
---     st <- ServerState <$> newIORef [
---         Note "Alice" "Must not forget to walk the dog."
---         , Note "Kaladin" "Are. You. Ready"
---         ]
---     cfg <- defaultSpockCfg () PCNoDatabase st
---     runSpock 8080 (spock cfg app)
 
 -- ----------------------- Posting new notes with a form------------------
 
@@ -111,6 +53,28 @@ app = do
             (notes <> [Note author contents], ())
         redirect "/"
 
+-- ----------------------- Rendering a Form----------------
+-- app :: Server ()
+-- app = do 
+--     get root $ do
+--         notes' <- getState >>= (liftIO . readIORef . notes)
+--         lucid $ do
+--             h1_ "Notes"
+--             ul_ $ forM_ notes' $ \note -> li_ $ do
+--                 toHtml (author note)
+--                 ": "
+--                 toHtml (contents note)
+--             h2_ "New Note"
+--             form_ [method_ "post"] $ do
+--                 label_ $ do
+--                     "Author: "
+--                     input_ [name_ "author"]
+--                 label_ $ do
+--                     "Contents: "
+--                     textarea_ [name_ "contents"] ""
+--                 input_ [type_ "submit", value_ "Add Note"]
+
+----------------------------------------------------
 
 main :: IO ()
 main = do
@@ -120,3 +84,66 @@ main = do
         ]
     cfg <- defaultSpockCfg () PCNoDatabase st
     runSpock 8080 (spock cfg app)
+
+
+
+-- ----------- Keeping Notes with Server State -------------------
+
+-- newtype ServerState = ServerState { notes :: IORef [Note] }
+-- data Note = Note {
+--     author      :: Text,
+--     contents    :: Text
+-- }
+
+-- type Server a = SpockM () () ServerState a
+
+-- app :: Server ()
+-- app = get root $ do
+--     notes' <- getState >>= (liftIO . readIORef . notes)
+--     lucid $ do
+--         h1_ "Notes"
+--         ul_ $ forM_ notes' $ \note -> li_ $ do
+--             toHtml (author note)
+--             ": "
+--             toHtml (contents note)
+
+
+-- main :: IO ()
+-- main = do
+--     st <- ServerState <$> newIORef [
+--         Note "Alice" "Must not forget to walk the dog."
+--         , Note "Kaladin" "Are. You. Ready"
+--         ]
+--     cfg <- defaultSpockCfg () PCNoDatabase st
+--     runSpock 8080 (spock cfg app)
+
+
+
+-- -------------- Show notes -----------------
+-- app :: SpockM () () () ()
+-- app = return ()
+
+-- type Server a = SpockM () () () a
+
+-- app :: Server ()
+-- app = return ()
+
+-- Defining Routes
+-- app :: Server ()
+-- app = get root (text "Hello!")
+
+-- Rendering HTML
+-- app :: Server ()
+-- app = get root (html "<h1>Hello!</h1>")
+
+-- Lucid
+-- app :: Server ()
+-- app = get root $ lucid $ do
+--     h1_ "Hello!"
+--     p_ "How are you today?"
+
+-- main :: IO ()
+-- main = do
+--     -- let app = return ()
+--     cfg <- defaultSpockCfg () PCNoDatabase ()
+--     runSpock 8080 (spock cfg app)
